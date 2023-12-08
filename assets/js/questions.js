1d4042efcf65d6856748687de05935b20202f5eb
David Flodin Bergqvist <david_flodin_bergqvist@live.se>
2023-12-08 15:01:09 +0000

    Add js file for questions

---
+++ assets/js/questions.js
@@ -0,0 +1,13 @@
+/* jshint esversion: 11 */
+const questions = [
+    {
+        question: 'What is the name of this exercise?',
+        image: 'squat.jpg',
+        answers: [
+            { text: 'Deadlift', correct: false},
+            { text: 'Squat', correct: true},
+            { text: 'Front Squat', correct: false},
+            { text: 'Goblet Squat', correct: false}
+        ]
+    }
+];
