bee4ea0a33c2a2f0d8ab101c91ef2d0c17cd8b0c
David Flodin Bergqvist <david_flodin_bergqvist@live.se>
2023-12-12 12:04:48 +0000

    Add question + image

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -9,5 +9,15 @@ const questions = [
             { text: 'Front Squat', correct: false},
             { text: 'Goblet Squat', correct: false}
         ]
+    },
+    {
+        question: 'What is the name of this exercise?',
+        image: 'deadlift.jpg',
+        answers: [
+            { text: 'Deadlift', correct: true},
+            { text: 'Squat', correct: false},
+            { text: 'Front Squat', correct: false},
+            { text: 'Goblet Squat', correct: false}
+        ]
     }
 ];
