2807f03b1c3dbb73cafef296e846d21199dbcc48
David Flodin Bergqvist <david_flodin_bergqvist@live.se>
2023-12-18 16:32:42 +0000

    Rename score-toggle and add localStorage

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,10 +2,20 @@
 showHideScore();
 
 /**
+ * Retrieves the data stored in localStorage,
+ * and replaces the innerText in the existing elements.
  * Shows/hides the last performance section
  * by adding/removing the .hide class on click.
  */
 function showHideScore() {
+    let totalQuestions = localStorage.getItem('total-questions');
+    let correctAnswers = localStorage.getItem('correct-answers');
+    let levelData = localStorage.getItem('level-data');
+
+    document.getElementById('total-questions').innerText = totalQuestions;
+    document.getElementById('correct-answers').innerText = correctAnswers;
+    document.getElementById('level').innerText = levelData;
+
     const score = document.getElementById('last-performance');
     const scoreToggle = document.getElementById('show-hide');
 
