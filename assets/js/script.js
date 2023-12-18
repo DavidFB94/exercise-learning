a07400afc5066906d423bd19a388794c4076a26f
David Flodin Bergqvist <david_flodin_bergqvist@live.se>
2023-12-18 15:54:35 +0000

    Add docstring to functions and rearrange order

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,12 @@ let shuffledQuestions, currentQuestionIndex, questions;
 resetButton.addEventListener('click', resetGame);
 startGame();
 
+/**
+ * Sets URL to represent the level,
+ * based on URL, selects which questions will be displayed
+ * randomizes questions order
+ * sets question with setNextQuestion()
+ */
 function startGame() {
     let currentURL = new URL(window.location.href);
     let currentLevel = currentURL.searchParams.get('level');
@@ -27,6 +33,11 @@ function startGame() {
     setNextQuestion();
 }
 
+/**
+ * Resets question state with resetQuestion(),
+ * Sets current question tracker and total question tracker, based on the total amount of questions
+ * displays question based on showQuestion()
+ */
 function setNextQuestion () {
     resetQuestion();
     document.getElementById('current-question').innerText = currentQuestionIndex + 1;
@@ -34,6 +45,13 @@ function setNextQuestion () {
     showQuestion(shuffledQuestions[currentQuestionIndex]);
 }
 
+/**
+ * Displays current question, together with the corresponding image,
+ * creates and displays the answer buttons in a randomized order,
+ * adds dataset correct to the correct answer/button,
+ * adds event listeners to buttons to run pickAnswer(),
+ * if on the last question of the quiz, change innerText of nextFinishButton to finish
+ */
 function showQuestion(question) {
     questionText.innerText = question.question;
     img.src = `assets/images/${question.image}`;
@@ -54,19 +72,9 @@ function showQuestion(question) {
     }
 }
 
-function resetQuestion() {
-    while (answerButtons.firstChild) {
-        answerButtons.removeChild(answerButtons.firstChild);
-    }
-}
-
-function pickAnswer(e) {
-    Array.from(answerButtons.children).forEach(button => {
-        setStatusClass(button, button.dataset.correct);
-    });
-    checkAnswer(e);
-}
-
+/**
+ * Adds the .correct or .wrong class to the buttons based on dataset
+ */
 function setStatusClass(element, correct) {
     clearStatusClass(element);
     if (correct) {
@@ -76,11 +84,45 @@ function setStatusClass(element, correct) {
     }
 }
 
+/** 
+* Clears the buttons of .correct/.wrong classes
+*/
 function clearStatusClass(element) {
     element.classList.remove('correct');
     element.classList.remove('wrong');
 }
 
+/**
+ * Disables nextFinishButton until an answer has been selected in the next question,
+ * increments currentQuestionIndex if there are more questions in the quiz,
+ * otherwise, it triggers endGame()
+ */
+nextFinishButton.addEventListener('click', () => {
+    nextFinishButton.classList.add('disable');
+    if (currentQuestionIndex < questions.length - 1) {
+        currentQuestionIndex++;
+        setNextQuestion();
+    } else {
+        endGame();
+    }
+});
+
+/**
+ * Checks for the correct answer,
+ * triggers checkAnswer()
+ */
+function pickAnswer(e) {
+    Array.from(answerButtons.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct);
+    });
+    checkAnswer(e);
+}
+
+/** 
+* Checks if the user picked the correct answer,
+* Increments trackers based on if the correct answer was selected,
+* removes the disable class from nextFinishButton to go to the next question.
+*/
 function checkAnswer(e) {
     let userAnswer = document.getElementsByClassName('correct')[0];
     if (e.target == userAnswer) {
@@ -93,30 +135,39 @@ function checkAnswer(e) {
     nextFinishButton.classList.remove('disable');
 }
 
-nextFinishButton.addEventListener('click', () => {
-    nextFinishButton.classList.add('disable');
-    if (currentQuestionIndex < questions.length - 1) {
-        currentQuestionIndex++;
-        setNextQuestion();
-    } else {
-        endGame();
-    }
-});
-
+/* Code Institutes Love Math project */
 function incrementRightAnswer() {
     let oldScore = parseInt(document.getElementById('correct-answers').innerText);
     document.getElementById('correct-answers').innerText = ++oldScore;
 }
 
+/* Code Institutes Love Math project */
 function incrementWrongAnswer() {
     let oldScore = parseInt(document.getElementById('incorrect-answers').innerText);
     document.getElementById('incorrect-answers').innerText = ++oldScore;
 }
 
+/**
+ * Removes answer buttons.
+ */
+function resetQuestion() {
+    while (answerButtons.firstChild) {
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+/**
+ * Reloads the quiz.
+ */
 function resetGame() {
     location.reload();
 }
 
+/* Built with help from youtube tutorial on localStorage() */
+/**
+ * Stores the tracked data in localStorage,
+ * redirects to score screen.
+ */
 function endGame () {
     let levelData = parseInt(document.getElementById('level').innerText);
     let correctAnswers = parseInt(document.getElementById('correct-answers').innerText);
